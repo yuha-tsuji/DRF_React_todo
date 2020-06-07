129b4653324583a4866d4efda73ee1521ba9ebc9
yuha-tsuji <yuga7517@gmail.com>
2020-06-07 10:11:56 +0900

    create method is created

--- todo_API/todo_API/settings.py
+++ todo_API/todo_API/settings.py
@@ -27,11 +27,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'api',
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- todo_front/src/components/DrfApi.js
+++ todo_front/src/components/DrfApi.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 const DrfApi = () => {
 
     const [todos, setTodos] = useState([])
+    const [editTodo, setEditTodo] = useState({id:'', title:'', content:''})
 
     useEffect(() => {
         try {
@@ -17,8 +18,45 @@ const DrfApi = () => {
         }
     }, [])
 
+    const createNewTodo = (todo) => {
+        const data = {
+            title: todo.title,
+            content: todo.content
+        }
+        try {
+            async function fetchCreateTodo(data){
+                const config = {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data)
+                }
+                const res = await fetch(`http://localhost:8000/api/todos/`, config)
+                const todoJson = await res.json()
+                setTodos([...todos, todoJson])
+                setEditTodo({id:'', title:'', content:''})
+            }
+            fetchCreateTodo(data)
+        } catch(e){
+            console.error(e)
+        }
+    }
+
+    const handleInputChange = () => e => {
+        const value=e.target.value;
+        const name = e.target.name
+        setEditTodo({...editTodo, [name]: value})
+    }
+
     return (
         <div>
+            <input type='text' name='title' placeholder='title' value={editTodo.title} onChange={handleInputChange()} required />
+            <br /><br />
+            <textarea name='content' placeholder='content' value={editTodo.content} onChange={handleInputChange()} required />
+            <br /><br />
+            <button onClick={()=>createNewTodo(editTodo)}>Create</button>
             <ul>
                 {
                     todos.map(todo => 
